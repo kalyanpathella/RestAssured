8f9eb7561c5b523c028f1b518dd61a04c111fc04
kalyanpathella <kalyanchakravarthi53@gmail.com>
2023-06-23 18:22:05 +0530

    my second commit

--- src/test/java/org/restAssured/Test2.java
+++ src/test/java/org/restAssured/Test2.java
@@ -6,11 +6,16 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		// BDD
+
+
 		given().baseUri("https://jsonplaceholder.typicode.com/posts")
 			.when()
 				.get()
+
+
 			.then()
 				.log().all();
 	}
+
 
 }
